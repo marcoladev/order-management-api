1283ea41c68cf2fc637c697d0310f631bf643592
marcoladev <108142358+marcoladev@users.noreply.github.com>
2026-06-13 15:57:15 -0300

    Removing unnecessary references

--- src/OrderManagement.Api/Middleware/ExceptionMiddleware.cs
+++ src/OrderManagement.Api/Middleware/ExceptionMiddleware.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using OrderManagement.Domain.Exceptions;
 
 namespace OrderManagement.Api.Middleware;
