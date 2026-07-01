be93414da473839efaae713fb2517eac32d4b2cc
marcoladev <108142358+marcoladev@users.noreply.github.com>
2026-06-30 22:41:37 -0300

    Decoupling the worker to another directory

--- src/OrderManagement.Worker/Program.cs
+++
@@ -1,9 +0,0 @@
-using OrderManagement.Worker;
-
-var builder = Host.CreateApplicationBuilder(args);
-
-builder.Services.AddWorkerServices(builder.Configuration);
-
-var host = builder.Build();
-
-host.Run();
